d9a46f8d52a41ac6c5deea4b306d78587f98ccbb
yun-heewon <union00916@gmail.com>
2026-04-06 18:39:52 +0900

    refactor: strictPropertyInitialization오류로 인한 dto 속성 수정

--- src/inquiry/dtos/create-update-reply.dto.ts
+++ src/inquiry/dtos/create-update-reply.dto.ts
@@ -3,5 +3,5 @@ import { IsString, IsNotEmpty } from 'class-validator';
 export class CreateOrUpdateInquiryReplyDto {
   @IsNotEmpty()
   @IsString({ message: '답변 내용은 문자열이어야 합니다.' })
-  content: string;
+  content!: string;
 }
